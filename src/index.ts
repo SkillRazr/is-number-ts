0e1cbbb5b8f4766980298a13602e5ae6163e8c3c
tukuna30 <tukuna.patro@gmail.com>
2022-10-24 09:46:45 +0530

    refactor code; add README

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 // Credits :- https://github.com/jonschlinkert/is-number/blob/master/index.js
 
 export function isNumber(num: any) {
-    // typeof NaN is 'number' but NaN !== NaN
-    if (num === Infinity || num === -Infinity) {
-        return false;
-    }
+    // typeof NaN is 'number' but NaN - NaN == NaN
+    // typeof Infinity is 'number but Infinity - Infinity === NaN
     if (typeof num === 'number') {
-        return num === num;
+        return num - num === 0;
     }
     if (typeof num === 'string') {
         if (num.trim() === '') { // empty strings => false
